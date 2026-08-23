beab7ed5d4230eff0e4ab406025ec8e51ca67bd7
Jonathan Daniels <j0nrages@users.noreply.github.com>
2026-08-23 13:27:51 -0500

    build(web): add Vercel ploy path deployment

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.PLOY_PUBLIC_BASE ?? "/",
   plugins: [react()],
   worker: {
     format: "es",
